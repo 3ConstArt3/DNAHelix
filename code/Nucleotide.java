b52e866bc21a8925f99576ea5d7be85279a7116f
ConstArt <114258174+3ConstArt3@users.noreply.github.com>
2024-04-30 16:18:54 +0300

    Create Nucleotide.java

---
+++ code/Nucleotide.java
@@ -0,0 +1,34 @@
+class Nucleotide
+{
+
+  private PVector position;
+  private float radius;
+
+  private color fillColor;
+
+  /* Constructor definition */
+  public Nucleotide(PVector position, float radius, color fillColor)
+  {
+    this.position = position;
+    this.radius = radius;
+    
+    this.fillColor = fillColor;
+  }
+
+  /* Function definition */
+  public void spin(PVector position)
+  {
+    this.position = position;
+  }
+
+  public void show()
+  {
+    pushMatrix();
+    translate(this.position.x, this.position.y);
+
+    stroke(this.fillColor);
+    fill(this.fillColor);
+    circle(0, 0, this.radius);
+    popMatrix();
+  }
+}
